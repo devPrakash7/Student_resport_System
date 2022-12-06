0b1a8beefd4af7d1feb9727bb5db76ac661ebfa0
devPrakash7 <psamantaray77@gmail.com>
2022-12-06 17:56:36 +0530

    init

---
+++ src/main/java/com/prakash/elastic/UIController.java
@@ -0,0 +1,51 @@
+package com.prakash.elastic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@Controller
+public class UIController {
+
+    @Autowired
+    private ElasticSearchQuery elasticSearchQuery;
+
+    @GetMapping("/")
+    public String viewHomePage(Model model) throws IOException {
+        model.addAttribute("listProductDocuments",elasticSearchQuery.searchAllDocuments());
+        System.out.println(elasticSearchQuery.searchAllDocuments());
+        return "index";
+    }
+
+    @PostMapping("/saveStudent")
+    public String saveProduct(@ModelAttribute("product") Student student) throws IOException {
+        elasticSearchQuery.createOrUpdateDocument(student);
+        return "redirect:/";
+    }
+
+    @GetMapping("/updateStudent/{id}")
+    public String showFormForUpdate(@PathVariable(value = "id") String id, Model model) throws IOException {
+
+        Student student = elasticSearchQuery.getDocumentById(id);
+        model.addAttribute("student", student);
+        return "updateStudent";
+    }
+
+    @GetMapping("/newStudent")
+    public String newStudent(Model model) {
+        // create model attribute to bind form data
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "newStudent";
+    }
+
+    @GetMapping("/deleteStudent/{id}")
+    public String deleteStudent(@PathVariable(value = "id") String id) throws IOException {
+
+        this.elasticSearchQuery.deleteDocumentById(id);
+        return "redirect:/";
+    }
+}
